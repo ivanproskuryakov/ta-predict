923bf106e7265c72ae17dd353a7562a459e79105
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-03-22 20:41:06 +0300

    price sequence estimation - p7

--- asset_read.py
+++ asset_read.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import numpy as np
 
 interval = Client.KLINE_INTERVAL_5MINUTE
-asset = 'ETH'
+asset = 'ADA'
 
 reader = reader.Reader()
 collection = reader.read(asset, interval)
@@ -18,6 +18,19 @@ negative = 0
 median = len(collection) / 2
 
 last_hour = 0
+last_hour_plus = 0
+last_hour_minus = 0
+
+def is_last_hour(last_hour, hour):
+    h = hour
+
+    if hour == 0:
+        h = 24
+
+    diff = abs(last_hour - h)
+
+    return diff == 4
+
 
 for sequence in collection:
     if len(sequence[1]):
@@ -26,10 +39,6 @@ for sequence in collection:
         percentage = []
         time = datetime.utcfromtimestamp(sequence[1][0]['time_open'])
 
-        if last_hour != time.hour:
-            last_hour = time.hour
-            print('')
-
         for item in sequence[1]:
             sum = sum + item['avg_percentage']
             percentage.append(item['avg_percentage'])
@@ -37,6 +46,7 @@ for sequence in collection:
         if is_positive:
             magnitude_positive = magnitude_positive + (len(sequence[1]))
             percentage_positive = percentage_positive + sum
+            last_hour_plus = last_hour_plus + sum
             print(
                 time,
                 percentage,
@@ -45,11 +55,22 @@ for sequence in collection:
         else:
             magnitude_negative = magnitude_negative + (len(sequence[1]))
             percentage_negative = percentage_negative + sum
+            last_hour_minus = last_hour_minus + sum
             print(
                 time,
                 percentage,
                 chalk.red(f'{np.round(sum, 2):.2f}'),
             )
+
+
+        if is_last_hour(last_hour, time.hour):
+            print(f'{last_hour_plus:.2f} {last_hour_minus:.2f}')
+            print(f'{last_hour_plus - (abs(last_hour_minus)):.2f}')
+            print('')
+            print('')
+            last_hour = time.hour
+            last_hour_plus = 0
+            last_hour_minus = 0
 
 print(
     magnitude_positive,
