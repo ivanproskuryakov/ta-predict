af1a7bce02d0ee7b23bfc6bf3f2a13bf7069b030
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-04-10 12:56:13 +0300

    price volatility visualisation

---
+++ matrix.py
@@ -0,0 +1,48 @@
+import json
+import sys
+
+from datetime import datetime
+from yachalk import chalk
+
+from parameters import assets as assets_available
+
+assets = assets_available[0:23]
+interval = sys.argv[1]
+collections = []
+
+for asset in assets:
+    with open(f'out_klines/{asset}_{interval}.json') as f:
+        data = f.read()
+        collection = json.loads(data)
+        f.close()
+        collections.append(collection)
+
+threshold = 0
+total = len(collections[0])
+
+line = f' -- date -- \t'
+
+for i in range(len(assets)):
+    line += f'{assets[i]} \t'
+
+print(line)
+
+for i in range(total):
+    time = collections[0][i]['time_open']
+    line = f'\n ' \
+           f'{datetime.utcfromtimestamp(time).strftime("%Y-%m-%d")} \t'
+
+    for a in range(len(assets)):
+        item = collections[a][i]
+
+        percentage = float(item['avg_percentage'])
+        change = f'{percentage:.1f}'
+
+        if percentage > threshold:
+            change = chalk.green(f'{percentage:.1f}')
+        if percentage < -threshold:
+            change = chalk.red(f'{percentage:.1f}')
+
+        line += f'{change} \t'
+
+    print(line)
--- readme.md
+++ readme.md
@@ -14,5 +14,6 @@ source ./venv/bin/activate
 python markets-dump.py
 python asset_dump.py
 python asset_read.py ETC
+python matrix 12h
 python ta.py ETC
 ```
