0c37cda221bf113f91cc2dad8014926432269ca8
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-04-27 22:19:53 +0300

    rnn data understanding

--- tf2.py
+++ tf2.py
@@ -1,76 +1,105 @@
-import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 import pandas as pd
 
-from sklearn.preprocessing import MinMaxScaler
 from binance import Client
+
+from service.window_generator import WindowGenerator
 from service.reader_ta import ReaderTA
 
-
-# Load data
+# Data load
 # ------------------------------------------------------------------------
 asset = 'ROSE'
 interval = Client.KLINE_INTERVAL_5MINUTE
 
 reader = ReaderTA()
-collection_original = reader.read(asset, interval)
-collection = collection_original[-4000:]
+collection = reader.read(asset, interval)
 prepared = []
 
 for i in range(0, len(collection)):
     prepared.append([
         # datetime.utcfromtimestamp(collection[i]['time_open']),
         collection[i]['price_open'],
-        # collection[i]['price_high'],
-        # collection[i]['price_low'],
-        # collection[i]['price_close'],
-        # collection[i]['volume'],
-        #
-        # collection[i]['avg_percentage'],
-        # collection[i]['trades'],
-        # collection[i]['volume_taker'],
+        collection[i]['price_high'],
+        collection[i]['price_low'],
+        collection[i]['price_close'],
+
+        collection[i]['volume'],
+        collection[i]['avg_percentage'],
+        collection[i]['trades'],
+        collection[i]['volume_taker'],
     ])
 
 df = pd.DataFrame(prepared, None, [
     # 'date',
     'open',
-    # 'high', 'low', 'close', 'volume',
-    # 'avg_percentage', 'trades', 'volume_taker'
+    'high',
+    'low',
+    'close',
+
+    'volume',
+    'avg_percentage',
+    'trades',
+    'volume_taker'
 ])
-pd.options.display.precision = 12
-np.set_printoptions(precision=12, suppress=True)
 
+print(len(df))
 
-target_names = [
-    'open',
-    # 'high', 'low', 'close', 'volume'
-]
+# Data split
+# --------------------------------------------------------
+n = len(df)
+train_df = df[0:int(n * 0.7)]
+val_df = df[int(n * 0.7):int(n * 0.9)]
+test_df = df[int(n * 0.9):]
 
+# Normalize the data
+# --------------------------------------------------------
 
-# Data preparation and scaling
-# ------------------------------------------------------------------------
+train_mean = train_df.mean()
+train_std = train_df.std()
 
-shift_steps = 5
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
 
-x_data = df.values[:-shift_steps] # cut tail, array size - 5
-y_data = df.values[shift_steps:] # cut head
+# Generator function
+# --------------------------------------------------------
 
+window = WindowGenerator(
+    input_width=30,
+    label_width=30,
+    shift=1,
+    batch_size=256,
+    label_columns=['open'],
+    train_df=train_df,
+    val_df=val_df,
+    test_df=test_df,
+)
 
-train_split = 0.9
-num_data = len(x_data)
-num_train = int(train_split * num_data)
-num_signals = x_data.shape[1]
+model = tf.keras.models.Sequential([
+    # Shape [batch, time, features] => [batch, time, lstm_units]
+    tf.keras.layers.LSTM(32, return_sequences=True),
+    # Shape => [batch, time, features]
+    tf.keras.layers.Dense(units=1)
+])
 
-x_train = x_data[0:num_train]
-x_test = x_data[num_train:]
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=2,
+    mode='min'
+)
+model.compile(
+    loss=tf.losses.MeanSquaredError(),
+    optimizer=tf.optimizers.Adam(),
+    metrics=[tf.metrics.MeanAbsoluteError()]
+)
+history = model.fit(
+    window.train,
+    epochs=20,
+    validation_data=window.val,
+    callbacks=[early_stopping]
+)
 
-y_train = y_data[0:num_train]
-y_test = y_data[num_train:]
+window.plot(model, 'open', 5)
 
-
-x_scaler = MinMaxScaler()
-x_train_scaled = x_scaler.fit_transform(x_train)
-x_test_scaled = x_scaler.transform(x_test)
-
-y_scaler = MinMaxScaler()
-y_train_scaled = y_scaler.fit_transform(y_train)
-y_test_scaled = y_scaler.transform(y_test)
+plt.show()
