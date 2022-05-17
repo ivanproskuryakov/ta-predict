835729a3d7afa0ccf6e97279e48425a771e0e085
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-05-17 23:40:06 +0600

    stable on test data

--- tf2_predict_plot_seen.py
+++ tf2_predict_plot_seen.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-from service.dataset_builder import build_dataset_prepared, build_dataset
+from service.dataset_builder import build_dataset_prepared
 from service.util import split_window
 from parameters import market, SIZE_BATCH, SIZE_SHIFT, ASSET, INTERVAL, SIZE_INPUT_LABEL
 
@@ -22,13 +22,7 @@ filepath_model = f'data/ta_{market}_{asset}_{interval}.keras'
     asset=asset,
     interval=interval
 )
-# df = build_dataset(
-#     market=market,
-#     asset=asset,
-#     interval=interval
-# )
-
-x = train_df
+x = test_df
 y = np.expand_dims(df, axis=0)
 
 # Model
@@ -64,44 +58,42 @@ ds = ds.map(lambda x: split_window(x, total_window_size, label_width, input_widt
 
 inputs, labels = next(iter(ds))
 
+predictions = model(inputs)
+
 # render
 # -------
 
+n = 0
+
 plt.figure(figsize=(12, 8))
+plt.subplot(1, 1, n + 1)
+plt.ylabel(f'open [normed]')
+plt.plot(
+    input_indices,
+    inputs[n, :, 0],
+    label='Inputs',
+    marker='.',
+    zorder=-10
+)
+plt.scatter(
+    label_indices,
+    labels[n, :, 0],
+    edgecolors='k',
+    label='Labels',
+    c='#2ca02c',
+    s=64
+)
+plt.scatter(
+    label_indices,
+    predictions[n, :, 0],
+    marker='X',
+    edgecolors='k',
+    label='Predictions',
+    c='#ff7f0e',
+    s=64
+)
 
-for n in range(max_subplots):
-    predictions = model(inputs)
-
-    plt.subplot(max_subplots, 1, n + 1)
-    plt.ylabel(f'open [normed]')
-    plt.plot(
-        input_indices,
-        inputs[n, :, 0],
-        label='Inputs',
-        marker='.',
-        zorder=-10
-    )
-    plt.scatter(
-        label_indices,
-        labels[n, :, 0],
-        edgecolors='k',
-        label='Labels',
-        c='#2ca02c',
-        s=64
-    )
-    plt.scatter(
-        label_indices,
-        predictions[n, :, 0],
-        marker='X',
-        edgecolors='k',
-        label='Predictions',
-        c='#ff7f0e',
-        s=64
-    )
-
-    if n == 0:
-        plt.legend()
-
+plt.legend()
 plt.xlabel('time')
 
 plt.show()
