2728eee05a67ded890bbebf6cbebb15f4e1e7a6e
Ivan <volgodark@gmail.com>
2022-06-20 16:49:50 +0300

    trained model

--- bitcoin-blocks-read.py
+++ bitcoin-blocks-read.py
@@ -1,8 +1,7 @@
 import os
-import json
 
 from datetime import datetime
-from blockchain_parser.blockchain import Blockchain
+from blockchain_parser.blockchain import Blockchain, Block, get_block
 
 DB_BITCOIN = '/home/ivan/.bitcoin/blocks'
 DB_BITCOIN_INDEX = '/home/ivan/.bitcoin/blocks/index'
@@ -11,8 +10,14 @@ collection = []
 start_at = datetime.now()
 blockchain = Blockchain(DB_BITCOIN)
 
-for block in blockchain.get_ordered_blocks(os.path.expanduser(DB_BITCOIN_INDEX), 0):
-    print(block)
-    print(block.header)
-    print(len(block.transactions))
-    exit()
+blocks = blockchain.get_ordered_blocks(
+    os.path.expanduser(DB_BITCOIN_INDEX)
+)
+
+block = Block(get_block('/home/ivan/.bitcoin/blocks/blk00001.dat', 31300506), 124392)
+
+for transaction in block.transactions:
+    for t_inputs in transaction.inputs:
+        print(t_inputs)
+    for t_outputs in transaction.outputs:
+        print(input)
--- src/service/reporter.py
+++ src/service/reporter.py
@@ -46,34 +46,3 @@ def render_console_table(report):
             ])
 
     print(tabulate(table, headers, tablefmt="simple", numalign="right"))
-
-
-def render_console(report):
-    for item in report:
-        asset, last_item, x_df, y_df = item
-
-        date = datetime.fromtimestamp(last_item["time_open"])
-
-        # Measure
-        last_real = x_df['open'].tail(1).values[0]
-
-        # 3 intervals
-        # predict what to be:
-        # 15min (5 x 3)
-        # 45min (15 x 3)
-        tail = y_df['open'].tail(4).values
-        last = tail[0]
-        prediction = tail[3]
-
-        diff = diff_percentage(v2=prediction, v1=last)
-
-        if diff > 0.6:
-            print(f''
-                  f'{asset} \t |'
-                  f'{paint_diff(diff)} \t |'
-                  f'{last_item["trades"]} \t |'
-                  f'{last_real:.4f} - {last:.4f} {tail[1]:.4f} {tail[2]:.4f} {tail[3]:.4f} \t | '
-                  f'{date.strftime("%Y %m %d %H:%M:%S")} - '
-                  f'https://www.binance.com/en/trade/{asset}_USDT')
-
-    print(datetime.now().strftime('%Y %m %d %H:%M:%S'))
