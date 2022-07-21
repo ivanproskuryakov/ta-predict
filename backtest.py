029d210c7e8bbf7f46072c9b95235fd964a39325
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-07-21 19:10:31 +0300

    scaling bugs - p2

--- backtest.py
+++ backtest.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from sklearn.preprocessing import MinMaxScaler
 from src.service.backtester import BackTester
+from src.service.util import diff_percentage
 
 scaler = MinMaxScaler()
 
@@ -29,14 +30,7 @@ x_df_rescaled = pd.DataFrame(rescaled, None, x_df.keys())
 x_last = x_df_rescaled.iloc[-2:]
 y_last = y_df.iloc[-2:]
 
-print('x_df_rescaled')
-print(x_df_rescaled)
+x_diff = diff_percentage(v2=x_df_rescaled.iloc[-1]['close'], v1=x_df_rescaled.iloc[-2]['close'])
+y_diff = diff_percentage(v2=y_last.iloc[-1]['close'], v1=y_last.iloc[-2]['close'])
 
-print('y_df')
-print(y_df)
-
-print('x_last')
-print(x_last)
-
-print('y_last')
-print(y_last)
+print(x_diff, y_diff)
