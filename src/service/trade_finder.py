9a6aaaba6727d1e7bea2d5e3628230479615381d
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-08-03 19:37:59 +0300

    rsi indicators

--- src/service/trade_finder.py
+++ src/service/trade_finder.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 class TradeFinder:
-    def pick_best_options(self, df: pd.DataFrame, diff: float, diff_sum: float) -> pd.DataFrame:
-        df = df.query(f'diff > {diff} and diff_sum < {diff_sum} and trades > 50')
+    def pick_best_options(self, df: pd.DataFrame, diff: float, rsi: float) -> pd.DataFrame:
+        df = df.query(f'diff > {diff} and rsi > {rsi} and trades > 50')
 
         df = df.sort_values(by=['diff_sum'], ascending=False)
 
@@ -11,8 +11,8 @@ class TradeFinder:
 
         return df
 
-    def pick_worst_options(self, df: pd.DataFrame, diff: float, diff_sum: float) -> pd.DataFrame:
-        df = df.query(f'diff < {diff} and diff_sum < {diff_sum} and trades > 50')
+    def pick_worst_options(self, df: pd.DataFrame, diff: float, rsi: float) -> pd.DataFrame:
+        df = df.query(f'diff < {diff} and rsi < {rsi} and trades > 50')
 
         df = df.sort_values(by=['diff'], ascending=False)
 
