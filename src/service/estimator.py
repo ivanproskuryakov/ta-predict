07ecaab2ff4fafcb78c15a17b0bc7a27319450b7
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-08-03 21:11:57 +0300

    ta indicators updated

--- src/service/estimator.py
+++ src/service/estimator.py
@@ -2,33 +2,61 @@ import talib.abstract as ta
 import src.vendor.qtpylib as qtpylib
 
 
+# https://mrjbq7.github.io/ta-lib/func_groups/overlap_studies.html
+
 def estimate_ta_fill_na(df):
+    price = qtpylib.typical_price(df)
+
     macd = ta.MACD(df)
-    bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(df), window=20, stds=2)
     stoch_fast = ta.STOCHF(df, 5, 3, 0, 3, 0)
 
-    # KST = ta.trend.KSTIndicator(
-    #     close=df['close'],
-    #     roc1=10,
-    #     roc2=15,
-    #     roc3=20,
-    #     roc4=30,
-    #     window1=10,
-    #     window2=10,
-    #     window3=10,
-    #     window4=15,
-    #     nsig=9,
-    #     fillna=False
-    # )
+    df_tdi = qtpylib.tdi(price)
+    df_heikinashi = qtpylib.heikinashi(df)
+    df_macd = qtpylib.macd(price)
+    df_weighted_bollinger_bands = qtpylib.weighted_bollinger_bands(price, window=20)
+    df_bollinger_bands = qtpylib.bollinger_bands(price, window=20)
+    df_keltner_channel = qtpylib.keltner_channel(df, window=14)
+    df_stoch = qtpylib.stoch(df, window=14)
+
+    df = df.join(df_tdi, how='right', lsuffix="_tdi_")
+    df = df.join(df_heikinashi, how='right', lsuffix="_heikinashi_")
+    df = df.join(df_macd, how='right', lsuffix="_macd_")
+    df = df.join(df_weighted_bollinger_bands, how='right', lsuffix="_weighted_bollinger_bands_")
+    df = df.join(df_bollinger_bands, how='right', lsuffix="_bollinger_bands_")
+    df = df.join(df_keltner_channel, how='right', lsuffix="_eltner_channel_")
+    df = df.join(df_stoch, how='right', lsuffix="_stoch_")
+
+    df['awesome_oscillator'] = qtpylib.awesome_oscillator(df)
+
+    df['atr'] = qtpylib.atr(df, window=14)
+    df['rolling_std'] = qtpylib.rolling_std(price, window=200)
+    df['rolling_mean'] = qtpylib.rolling_mean(price, window=200)
+    df['rolling_min'] = qtpylib.rolling_min(price, window=14)
+    df['rolling_weighted_mean'] = qtpylib.rolling_weighted_mean(price, window=200)
+    df['hull_moving_average'] = qtpylib.hull_moving_average(price, window=200)
+
+    df['sma'] = qtpylib.sma(price, window=200)
+    df['wma'] = qtpylib.wma(price, window=200)
+    df['hma'] = qtpylib.hma(price, window=200)
+    df['vwap'] = qtpylib.vwap(df)
+
+    df['rolling_vwap'] = qtpylib.rolling_vwap(df, window=200)
+    df['rsi'] = qtpylib.rsi(price, window=14)
+    df['cci'] = qtpylib.cci(df, window=14)
+
+    # df['roc'] = qtpylib.roc(df, window=14)
+    # df['zlma'] = qtpylib.zlma(df, window=14)
+    # df['zlema'] = qtpylib.zlema(df, window=14)
+    # df['zlsma'] = qtpylib.zlsma(df, window=14)
+    # df['zlhma'] = qtpylib.zlhma(df, window=14)
+
+    df['zscore'] = qtpylib.zscore(df, window=20)
+    df['pvt'] = qtpylib.pvt(df)
+    df['chopiness'] = qtpylib.chopiness(df)
 
     tadf = {
         'adx': ta.ADX(df, timeperiod=14),
         'cdlhammer': ta.CDLHAMMER(df),
-
-        # bollinger
-        'bollinger_up': bollinger['upper'],
-        'bollinger_mid': bollinger['mid'],
-        'bollinger_low': bollinger['lower'],
 
         # CCI
         # 'trend_kst_diff': KST.kst_diff(),
@@ -36,9 +64,6 @@ def estimate_ta_fill_na(df):
         'cci_one': ta.CCI(df, timeperiod=170),
         'cci_two': ta.CCI(df, timeperiod=34),
         'cci': ta.CCI(df),
-
-        'rsi': ta.RSI(df, timeperiod=14),
-        'TR': ta.TRANGE(df),
 
         # EMA - Exponential Moving Average
         'ema_5': ta.EMA(df, timeperiod=5),
@@ -48,9 +73,9 @@ def estimate_ta_fill_na(df):
         'ema_low': ta.EMA(df, timeperiod=5, price='low'),
 
         # MACD
-        'macd': macd['macd'],
-        'macd_signal': macd['macdsignal'],
-        'macd_hist': macd['macdhist'],
+        '__macd': macd['macd'],
+        '__macd_signal': macd['macdsignal'],
+        '__macd_hist': macd['macdhist'],
 
         'mom': ta.MOM(df, timeperiod=14),
         'mfi': ta.MFI(df),
@@ -74,13 +99,45 @@ def estimate_ta_fill_na(df):
         'sma_long': ta.SMA(df, timeperiod=6),
         'sma_fastMA': ta.SMA(df, timeperiod=14),
         'sma_slowMA': ta.SMA(df, timeperiod=28),
-
-        # TEMA
-        'tema': ta.TEMA(df, timeperiod=9),
     }
 
     for key in tadf.keys():
         df[key] = tadf[key]
+
+    # Overlap Studies Functions
+    # https://mrjbq7.github.io/ta-lib/func_groups/overlap_studies.html
+    df['DEMA'] = ta.DEMA(df, timeperiod=30)
+    df['EMA'] = ta.EMA(df, timeperiod=5)
+    df['ADXR'] = ta.ADXR(df, timeperiod=5)
+    df['KAMA'] = ta.KAMA(df, timeperiod=30)
+    df['MA'] = ta.ADXR(df, timeperiod=30, matype=0)
+
+    df_MAMA = ta.MAMA(df)
+    df = df.join(df_MAMA, how='right', lsuffix="_MAMA_")
+
+    df['MIDPOINT'] = ta.MIDPOINT(df, timeperiod=14)
+    df['MIDPRICE'] = ta.MIDPRICE(df['high'], df['low'], timeperiod=14)
+    df['SAREXT'] = ta.SAREXT(df['high'], df['low'])
+    df['SMA'] = ta.SMA(df['close'], timeperiod=30)
+    df['T3'] = ta.T3(df['close'], timeperiod=5)
+    df['TEMA_9'] = ta.TEMA(df['close'], timeperiod=9)
+    df['TEMA'] = ta.TEMA(df['close'], timeperiod=30)
+    df['TRIMA'] = ta.TRIMA(df['close'], timeperiod=30)
+    df['WMA'] = ta.WMA(df['close'], timeperiod=30)
+
+    # Volume Indicator Functions
+    # https://mrjbq7.github.io/ta-lib/func_groups/volume_indicators.html
+
+    df['AD'] = ta.AD(df['high'], df['low'], df['close'], df['volume'])
+    df['ADOSC'] = ta.ADOSC(df['high'], df['low'], df['close'], df['volume'], fastperiod=3, slowperiod=10)
+    df['ADOSC'] = ta.OBV(df['close'], df['volume'])
+
+    # Volatility Indicator Functions
+    # https://mrjbq7.github.io/ta-lib/func_groups/volatility_indicators.html
+
+    df['ATR'] = ta.ATR(df['high'], df['low'], df['close'], timeperiod=14)
+    df['NATR'] = ta.NATR(df['high'], df['low'], df['close'], timeperiod=14)
+    df['TRANGE'] = ta.TRANGE(df['high'], df['low'], df['close'])
 
     df = df.fillna(0)
 
