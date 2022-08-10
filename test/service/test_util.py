5e61a8de4f53902878941e42ce315bed8ba7d56a
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-08-10 18:46:59 +0300

    cleaning app from non-relevant to ohlc training and prediction

--- test/service/test_util.py
+++ test/service/test_util.py
@@ -3,39 +3,7 @@ from src.service.util import Utility
 utility = Utility()
 
 
-def test_price_hight_btc():
-    bnb = 0.011
-    ada = 0.000023
-    trx = 0.000003
-    celo = 0.000045
+def test_diff_percentage():
+    diff = utility.diff_percentage(v1=0.001, v2=0.0004)
 
-    diff_ada = utility.diff_price(ada)
-    diff_bnb = utility.diff_price(bnb)
-    diff_trx = utility.diff_price(trx)
-    diff_celo = utility.diff_price(celo)
-
-    assert diff_ada == 434782608.6956522
-
-    assert diff_bnb * bnb == 10000.0
-    assert diff_ada * ada == 10000.0
-    assert diff_trx * trx == 10000.0
-    assert diff_celo * celo == 10000.0
-
-
-def test_price_hight_usdt():
-    bnb = 200
-    ada = 0.4
-    trx = 0.01
-    celo = 0.8
-
-    diff_ada = utility.diff_price(ada)
-    diff_bnb = utility.diff_price(bnb)
-    diff_trx = utility.diff_price(trx)
-    diff_celo = utility.diff_price(celo)
-
-    assert diff_ada == 25000.0
-
-    assert diff_bnb * bnb == 10000.0
-    assert diff_ada * ada == 10000.0
-    assert diff_trx * trx == 10000.0
-    assert diff_celo * celo == 10000.0
+    assert diff == -85.7143
